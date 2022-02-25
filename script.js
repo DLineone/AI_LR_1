55c62d566e3c4a7be222247f7d88dd12930e031e
DLineone <DLineone@users.noreply.github.com>
2022-02-26 00:47:43 +0300

    new

--- script.js
+++ script.js
@@ -90,7 +90,100 @@ function InitInsert()
     let correctSymptomBut = document.querySelector(".correct-illnes");
     
     insertSymptomBut.onclick = function(){InitInsertSymptom()};
-    insertIllnesBut.onclick = function(){InitInsertIllnes(0, [])}
+    insertIllnesBut.onclick = function(){InitInsertIllnes(0, [])};
+    correctSymptomBut.onclick = function(){InitCorrectIllnes()};
+}
+
+function InitCorrectIllnes()
+{
+    insertContent.innerHTML = " ";
+    for(let i = 0; i < Nillnes; i++)
+    {
+        let btn = `<button class="illnes${i}" onclick = "InitCorrectIllnesIterable(0, [], ${i})">${illnes[i]}</button>`;
+        insertContent.innerHTML += btn;
+    }
+}
+
+function InitCorrectIllnesIterable(iterator, ansver, iIllnes)
+{
+    console.log(iIllnes);
+    insertContent.innerHTML = `<div class="question">Эта болезнь характеризуется этим симптомом: "${symptoms[iterator]}"?</div>
+        <div class="ansver">
+            <button class="yes">Да</button>
+            <button class="no">Нет</button>
+        </div>`;
+    let buttonYes = document.querySelector(".yes");
+    let buttonNo = document.querySelector(".no");
+
+    buttonYes.onclick = function (){
+        ansver.push(1);
+        iterator++;
+
+        if(iterator < Nsymptoms) InitCorrectIllnesIterable(iterator, ansver, iIllnes);
+        else CorrectCheck(ansver, iIllnes);
+    }
+
+    buttonNo.onclick = function (){
+        ansver.push(0);
+        iterator++;
+
+        if(iterator < Nsymptoms) InitCorrectIllnesIterable(iterator, ansver, iIllnes);
+        else CorrectCheck(ansver, iIllnes);
+    }
+}
+
+function CorrectCheck(ansver, iIllnes)
+{
+    for(let i = 0; i < Nillnes; i++)
+    {
+        let Nsymptoms1 = 0;
+        let Nsymptoms2 = 0;
+        let equals = 0;
+
+        for(let j = 0; j < Nsymptoms; j++)
+        {
+            if(i != iIllnes){
+                Nsymptoms1 = (ansver[j] === 1) ? Nsymptoms1 + 1 : Nsymptoms1;
+                Nsymptoms2 = (diagnoses[i][j] === 1) ? Nsymptoms2 + 1 : Nsymptoms2;
+                equals = (ansver[j] === diagnoses[i][j] && diagnoses[i][j] === 1 && ansver[j] === 1) ? equals + 1 : equals;
+            }
+        }
+
+        if((Nsymptoms1 > Nsymptoms2 && Nsymptoms2 === equals) || (Nsymptoms2 > Nsymptoms1 && Nsymptoms1 === equals) || (Nsymptoms1 === Nsymptoms2 && Nsymptoms1 === equals))
+        {
+            WrongCorrectError();
+            return 0;
+        }
+    }
+    CompleteCorrectIllnes(ansver, iIllnes);
+}
+
+function WrongCorrectError()
+{
+    insertContent.innerHTML = `<p>Данная болезнь не соответствует условию (возможно она является вхождением другой болезни).</p>
+    <button class = "backToInsert">Попробовать снова</button>`
+    let button1 = document.querySelector(".backToInsert");
+    button1.onclick = function(){InitInsertIllnes(0, [])};
+}
+
+function CompCorrectIllnes(ansver, iIllnes)
+{
+    insertContent.innerHTML = `            <p>Введите название болезни</p>
+    <input type="text" class = "illnes-name">
+    <button class="submit-illnes">Добавить болезнь</button>`;
+
+    let inputIllnesName = document.querySelector(".illnes-name");
+    let inputSubmitButton = document.querySelector(".submit-illnes");
+
+    inputSubmitButton.onclick = function(){
+        Nillnes++;
+        illnes[iIllnes] = inputIllnesName.value;
+        for(let i = 0; i < Nsymptoms; i++)
+        {
+            diagnoses[i][iIllnes] = ansver[i];
+        }
+        toInsert();
+    };
 }
 
 function InitInsertSymptom()
@@ -115,7 +208,7 @@ function InitInsertSymptom()
     }
 }
 
-//change
+
 function InitInsertIllnes(iterator, ansver)
 {
     insertContent.innerHTML = `<div class="question">Эта болезнь характеризуется этим симптомом: "${symptoms[iterator]}"?</div>
@@ -265,6 +358,5 @@ function showAnsver(ansver)
 
         let toTestButtonAns = document.querySelector(".toTestButtonAns");
         toTestButtonAns.onclick = function () {toTest()};
-
     }
 }
